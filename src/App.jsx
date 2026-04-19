aef74f42a7dbe935ebcccddc315fa08576bdef3a
Diego-Back-End <escobedoambrosettidiego@gmail.com>
2026-04-19 17:28:29 -0400

    feat: estructura inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Registro from './pages/Registro'
+import Catalogo from './pages/Catalogo'
+import DetalleProducto from './pages/DetalleProducto'
+import Carrito from './pages/Carrito'
+import Checkout from './pages/Checkout'
+import MisPedidos from './pages/MisPedidos'
+import Notificaciones from './pages/Notificaciones'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Catalogo />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/producto/:id" element={<DetalleProducto />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/mis-pedidos" element={<MisPedidos />} />
+        <Route path="/notificaciones" element={<Notificaciones />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
